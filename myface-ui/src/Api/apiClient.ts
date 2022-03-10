248af76a86cc1160b5cc0cadf3fa48a24f82ea7d
Deepashri Dali <deepashri@hotmail.com>
2022-03-10 11:40:42 +0000

    Add auth to fetchPostsForUser, fetchPostsLiked and Disliked

--- myface-ui/src/Api/apiClient.ts
+++ myface-ui/src/Api/apiClient.ts
@@ -65,18 +65,33 @@ export async function fetchPosts(page: number, pageSize: number): Promise<ListRe
     return await response.json();
 }
 
-export async function fetchPostsForUser(page: number, pageSize: number, userId: string | number) {
-    const response = await fetch(`https://localhost:5001/feed?page=${page}&pageSize=${pageSize}&postedBy=${userId}`);
+export async function fetchPostsForUser(page: number, pageSize: number, userId: string | number, username: string, password: string) {
+    const response = await fetch(`https://localhost:5001/feed?page=${page}&pageSize=${pageSize}&postedBy=${userId}`, {
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": getAuthorizationHeader(username, password),
+        },
+    });
     return await response.json();
 }
 
-export async function fetchPostsLikedBy(page: number, pageSize: number, userId: string | number) {
-    const response = await fetch(`https://localhost:5001/feed?page=${page}&pageSize=${pageSize}&likedBy=${userId}`);
+export async function fetchPostsLikedBy(page: number, pageSize: number, userId: string | number, username: string, password: string) {
+    const response = await fetch(`https://localhost:5001/feed?page=${page}&pageSize=${pageSize}&likedBy=${userId}`, {
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": getAuthorizationHeader(username, password),
+        },
+    });
     return await response.json();
 }
 
-export async function fetchPostsDislikedBy(page: number, pageSize: number, userId: string | number) {
-    const response = await fetch(`https://localhost:5001/feed?page=${page}&pageSize=${pageSize}&dislikedBy=${userId}`);
+export async function fetchPostsDislikedBy(page: number, pageSize: number, userId: string | number, username: string, password: string) {
+    const response = await fetch(`https://localhost:5001/feed?page=${page}&pageSize=${pageSize}&dislikedBy=${userId}`, {
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": getAuthorizationHeader(username, password),
+        },
+    });
     return await response.json();
 }
 
--- myface-ui/src/Pages/Profile/Profile.tsx
+++ myface-ui/src/Pages/Profile/Profile.tsx
@@ -7,11 +7,11 @@ import {PostList} from "../../Components/PostList/PostList";
 import {fetchPostsDislikedBy, fetchPostsForUser, fetchPostsLikedBy} from "../../Api/apiClient";
 import "./Profile.scss";
 import {Users} from "../Users/Users";
-
+import {LoginContext} from '../../Components/LoginManager/LoginManager';
 
 export function Profile(): JSX.Element {
     const {id} = useParams<{id:string | undefined}>();
- 
+    const { userName, password } = useContext(LoginContext);
     
     if (id === undefined) {
         // Shouldn't ever happen - but if the ID is somehow undefined, show the base users page.
@@ -22,9 +22,9 @@ export function Profile(): JSX.Element {
         <Page containerClassName="profile">
             <UserDetails userId={id!}/>
             <div className="activity">
-                <PostList title="Posts" fetchPosts={() => fetchPostsForUser(1, 12, id)}/>
-                <PostList title="Likes" fetchPosts={() => fetchPostsLikedBy(1, 12, id)}/>
-                <PostList title="Dislikes" fetchPosts={() => fetchPostsDislikedBy(1, 12, id)}/>
+                <PostList title="Posts" fetchPosts={() => fetchPostsForUser(1, 12, id, userName as string , password as string)}/>
+                <PostList title="Likes" fetchPosts={() => fetchPostsLikedBy(1, 12, id,userName as string , password as string)}/>
+                <PostList title="Dislikes" fetchPosts={() => fetchPostsDislikedBy(1, 12, id, userName as string , password as string)}/>
             </div>
         </Page>
     );
